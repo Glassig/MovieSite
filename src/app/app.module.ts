0d3b6876dfe820b5a0098fef80a3722e473697a1
Robert Lorentz <robey112@live.se>
2017-03-23 21:53:48 +0100

    service and search added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
+
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 import { AppComponent } from './app.component';
+import { SearchMovieComponent } from './search-movie/search-movie.component';
+import { HeaderComponent } from './header/header.component';
+
+import { MovieService } from './movie.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchMovieComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    MovieService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/model/movie.ts
@@ -0,0 +1,6 @@
+export class Movie {
+  id: number;
+  title: string;
+  imageUrl: string;
+  overview: string;
+}
