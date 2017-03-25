b95fc1eca8623b36c41bc040d08295d75da63411
Robert Lorentz <robey112@live.se>
2017-03-25 19:45:03 +0100

    added support for databases in firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -43,7 +43,7 @@ import { MyProfileComponent } from './my-profile/my-profile.component';
     MaterialModule,
     AppRoutingModule,
     FlexLayoutModule,
-    AngularFireModule.initializeApp(firebaseConfig)
+    AngularFireModule.initializeApp(firebaseConfig),
   ],
   providers: [
     ApiService,
--- src/app/providers/af.ts
+++ src/app/providers/af.ts
@@ -1,15 +1,23 @@
 import {Injectable} from "@angular/core";
-import {AngularFire, AuthProviders, AuthMethods} from 'angularfire2';
+import {AngularFire, AuthProviders, AuthMethods, FirebaseListObservable} from 'angularfire2';
 import { User } from '../model/user';
+import { Movie } from '../model/movie';
+
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
 @Injectable()
 export class AF {
 
-
+  public watchlist: FirebaseListObservable<any>;
   isLoggedIn: boolean = false;
   public user: User;
+  users: FirebaseListObservable<any>;
 
   constructor(public af: AngularFire) {
     this.user = new User();
+    this.watchlist = this.af.database.list('watchlist');
+    this.users = this.af.database.list('users');
   }
   /**
    * Logs in the user
@@ -24,13 +32,25 @@ export class AF {
     .then((data) => {
       this.user = this.userFromJson(data);
       this.isLoggedIn = true;
+      var add = true;
+      this.users
+      .subscribe(resp => {
+        resp.forEach(user => {
+          if(user.id == this.user.id) { add = false }
+        });
+        if(add) {
+          this.users.push(this.user);
+        }
+      })
+
+
     });
-  }
+    }
 
   userFromJson(json: any): User {
     var user = new User();
     user.email = json.auth.email;
-    user.id = +json.uid;
+    user.id = json.auth.uid;
     user.imageUrl = json.auth.photoURL;
     user.name = json.auth.displayName;
     return user;
@@ -42,5 +62,16 @@ export class AF {
     this.isLoggedIn = false;
     this.user = new User();
     return this.af.auth.logout();
+  }
+
+  addMovieToWatchlist(movie: Movie) {
+    var watchListMovie = {
+      id: movie.id,
+      uid: this.user.id,
+      imageUrl: movie.imageUrl,
+      title: movie.title,
+      overview: movie.overview
+    }
+    this.watchlist.push(watchListMovie);
   }
 }
