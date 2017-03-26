360d036c366756ec8ad421be5f01c7c87353a0ab
Oskar Ek <oskarek24@gmail.com>
2017-03-26 23:46:20 +0200

    added a few properties to Movie class

--- src/app/model/movie.ts
+++ src/app/model/movie.ts
@@ -3,4 +3,9 @@ export class Movie {
   title: string;
   imageUrl: string;
   overview: string;
+  collectionId: number | undefined;
+  // maybe make genre an enum in future
+  genres: string[];
+  runtime: string;
+  releaseDate: string;
 }
